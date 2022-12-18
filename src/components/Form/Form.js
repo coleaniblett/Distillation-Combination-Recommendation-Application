c13a15d50f704dc38903f8b8c34e335395161cde
ColeANiblett <coleaniblett@gmail.com>
2022-12-18 14:56:23 -0800

    move variable definition to render method

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 import {Ingredients} from '../Ingredients/Ingredients';
 
 const {liquors, liqueurs, mixers} = Ingredients;
 
-export const Form = () => {
-
-  const liquorSelect = document.getElementById("liquor");
+export const Form = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({});
+  /*const liquorSelect = document.getElementById("liquor");
   const liqueurSelect = document.getElementById("liqueur");
-  const mixerSelect = document.getElementById("mixer");
+  const mixerSelect = document.getElementById("mixer");*/
+  const [liquorOptions, setLiquorOptions] = useState([]);
+  const [liqueurOptions, setLiqueurOptions] = useState([]);
+  const [mixerOptions, setMixerOptions] = useState([]);
 
-  if (liquorSelect.childElementCount === 1) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(formData);
+    window.alert("Test");
+  }
+
+  /*f (liquorSelect.childElementCount === 1) {
     liquors.map(option => {
       const optionElement = document.createElement('option');
       optionElement.value = option;
@@ -34,26 +43,35 @@ export const Form = () => {
       mixerSelect.appendChild(optionElement);
       return "";
     });
+  }*/
+  if (liquorOptions.length === 0) {
+    setLiquorOptions(liquors.map(option => <option value={option}>{option}</option>));
+    setLiqueurOptions(liqueurs.map(option => <option value={option}>{option}</option>));
+    setMixerOptions(mixers.map(option => <option value={option}>{option}</option>));
   }
+
 
   return (
     <form id="cocktail-form">
       <label for="liquor">Liquor:</label><br/>
       <select id="liquor" className="liquor">
         <option value=""></option>
+        {liquorOptions}
       </select><br/>
       <br/>
       <label for="liqueur">Liqueur:</label><br/>
       <select id="liqueur" className="liqueur">
-        <option value=""></option>  
+        <option value=""></option>
+        {liqueurOptions}
       </select><br/>
       <br/>
       <label for="mixer">Mixer:</label><br/>
       <select id="mixer" className="mixer">
         <option value=""></option>
+        {mixerOptions}
       </select><br/>
       <br/>
-      <button type="submit">Add ingredients to inventory</button><br/>
+      <button type="submit" >Add ingredients to inventory</button><br/>
       <br/>
       <button type="submit">Submit inventory</button>
 
