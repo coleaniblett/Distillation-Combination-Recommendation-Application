d9ec2821b65c992c4c6d43a62619b019fda3e237
ColeANiblett <coleaniblett@gmail.com>
2022-12-20 12:59:26 -0800

    simplify menu input to one dropdown

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,80 +1,67 @@
 import React, { useState } from 'react';
 import './Form.css';
 import {Ingredients} from '../Ingredients/Ingredients';
+import {Dropdown} from '../Dropdown/Dropdown'
 
 const {liquors, liqueurs, mixers} = Ingredients;
 
 export const Form = ({ onSubmit }) => {
   const [formData, setFormData] = useState({});
-  /*const liquorSelect = document.getElementById("liquor");
-  const liqueurSelect = document.getElementById("liqueur");
-  const mixerSelect = document.getElementById("mixer");*/
   const [liquorOptions, setLiquorOptions] = useState([]);
   const [liqueurOptions, setLiqueurOptions] = useState([]);
   const [mixerOptions, setMixerOptions] = useState([]);
+  const [selectionName, setSelectionName] = useState("Liquors");
 
   function handleSubmit(event) {
     event.preventDefault();
-    onSubmit(formData);
-    window.alert("Test");
+    onSubmit({ ...formData, id: Date.now() });
   }
 
-  /*f (liquorSelect.childElementCount === 1) {
-    liquors.map(option => {
-      const optionElement = document.createElement('option');
-      optionElement.value = option;
-      optionElement.textContent = option;
-      liquorSelect.appendChild(optionElement);
-      return "";
-    });
-  
-    liqueurs.map(option => {
-      const optionElement = document.createElement('option');
-      optionElement.value = option;
-      optionElement.textContent = option;
-      liqueurSelect.appendChild(optionElement);
-      return "";
-    });
-  
-    mixers.map(option => {
-      const optionElement = document.createElement('option');
-      optionElement.value = option;
-      optionElement.textContent = option;
-      mixerSelect.appendChild(optionElement);
-      return "";
-    });
-  }*/
+  function handleChange(event) {
+    const {className, value} = event.target;
+    if (value != "") {
+      setFormData(prevFormData => ({ ...prevFormData, ["name"]: value}));
+    }
+  }
+
+   function handleSelection(event) {
+     const {id} = event.target;
+     setSelectionName(id);
+   }
+
   if (liquorOptions.length === 0) {
-    setLiquorOptions(liquors.map(option => <option value={option}>{option}</option>));
-    setLiqueurOptions(liqueurs.map(option => <option value={option}>{option}</option>));
-    setMixerOptions(mixers.map(option => <option value={option}>{option}</option>));
+    setLiquorOptions(liquors.map(option => <option className="liquor" value={option}>{option}</option>));
+    setLiqueurOptions(liqueurs.map(option => <option className="liqueur" value={option}>{option}</option>));
+    setMixerOptions(mixers.map(option => <option className="mixer" value={option}>{option}</option>));
   }
-
 
   return (
     <form id="cocktail-form">
-      <label for="liquor">Liquor:</label><br/>
-      <select id="liquor" className="liquor">
+      <button className="selection-button" onClick={handleSelection} id="Liquors">Liquor</button>
+      <button className="selection-button" onClick={handleSelection} id="Liqueurs">Liqueur</button>
+      <button className="selection-button" onClick={handleSelection}>Mixer</button>
+      {/*<label for="liquor">Liquor:</label><br/>
+      <select id="liquor" className="liquor" onChange={handleChange}>
         <option value=""></option>
         {liquorOptions}
       </select><br/>
       <br/>
       <label for="liqueur">Liqueur:</label><br/>
-      <select id="liqueur" className="liqueur">
+      <select id="liqueur" className="liqueur" onChange={handleChange}>
         <option value=""></option>
         {liqueurOptions}
       </select><br/>
       <br/>
       <label for="mixer">Mixer:</label><br/>
-      <select id="mixer" className="mixer">
+      <select id="mixer" className="mixer" onChange={handleChange}>
         <option value=""></option>
         {mixerOptions}
       </select><br/>
-      <br/>
-      <button type="submit" >Add ingredients to inventory</button><br/>
+      <br/>*/}
+      <Dropdown choices={liqueurOptions} choicesName={selectionName} selectIngredient={handleChange}/>
+      <button type="submit" onClick={handleSubmit}>Add ingredients to inventory</button><br/>
       <br/>
       <button type="submit">Submit inventory</button>
-
     </form>
   );
 }
