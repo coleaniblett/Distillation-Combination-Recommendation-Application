532b4c810125fc5d541ffb5e74a4317010ad0a1f
ColeANiblett <coleaniblett@gmail.com>
2023-02-13 23:34:04 -0800

    implement remove-button class

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,20 +3,22 @@ import './Inventory.css';
 
 export const Inventory = ({ ingredients, onRemoveIngredient }) => {
   return (
-    <div id="Inventory" className="text-box">
-      <h2 className="section-heading">Inventory</h2>
-      <ul>
-        <li>
-          Water
-          <button onClick={() => window.alert("If you don't have water, maybe you shouldn't be drinking?")}>X</button>
-        </li>
-        {ingredients.map(ingredient => (
-          <li key={ingredient.id}>
-            {ingredient.name}
-            <button onClick={() => onRemoveIngredient(ingredient.id)}>X</button>
+    <div className="inventory">
+      <div className="text-box">
+        <h2 className="section-heading">Inventory</h2>
+        <ul className="inventory-list">
+          <li>
+            Water
+            <button className="remove-button" onClick={() => window.alert("If you don't have water, maybe you shouldn't be drinking?")}>X</button>
           </li>
-        ))}
-    </ul>
+          {ingredients.map(ingredient => (
+            <li key={ingredient.id}>
+              {ingredient.name}
+              <button className="remove-button" onClick={() => onRemoveIngredient(ingredient.id)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
