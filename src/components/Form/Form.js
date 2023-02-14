d6179a8742e0f94260a9709d1aa044fb27169123
ColeANiblett <coleaniblett@gmail.com>
2023-02-13 23:08:59 -0800

    implement FormButton component, rename variables

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,72 +1,58 @@
 import './Form.css';
 import React, { useState } from 'react';
 import { Ingredients } from '../Ingredients/Ingredients';
-import { Dropdown } from '../Dropdown/Dropdown'
+import { Dropdown } from '../Dropdown/Dropdown';
+import { FormButton } from '../FormButton/FormButton';
 
 const {liquors, beers, wines, liqueurs, mixers, garnishes, other} = Ingredients;
-const categories = [
-  "Liquors",
-  "Beers",
-  "Wines",
-  "Liqueurs",
-  "Mixers",
-  "Garnishes",
-  "Other"
-];
+const categories = {
+  "Liquors": liquors,
+  "Beers": beers,
+  "Wines": wines,
+  "Liqueurs": liqueurs,
+  "Mixers": mixers,
+  "Garnishes": garnishes,
+  "Other": other
+}
 
 export const Form = ({ onAdd, onSubmit }) => {
   const [formData, setFormData] = useState({});
-  const [selectionName, setSelectionName] = useState("Liquors");
-  const [selection, setSelection] = useState(liquors);
+  const [categoryName, setCategoryName] = useState("Liquors");
+  const [category, setCategory] = useState(liquors);
 
-  function handleSubmit(event) {
+  function handleCategoryChange(event) {
+    const id = event.target.value;
+    setCategoryName(id);
+    setCategory(categories[`${id}`]);
+  }
+
+  function handleIngredientChange(event) {
+    const {value} = event.target;
+    setFormData(prevFormData => ({ ...prevFormData, "name": value}));
+  }
+
+  const handleIngredientSubmit = (event) => {
     event.preventDefault();
     if (formData.name === undefined || formData.name === "") {
       window.alert("No option selected");
     }
     else {
       onAdd({ ...formData, id: Date.now() });
-      formData.name = "";
     }
   }
 
-  function handleChange(event) {
-    const {value} = event.target;
-    setFormData(prevFormData => ({ ...prevFormData, "name": value}));
-  }
-
-   function handleSelection(event) {
-    const id = event.target.value;
-    setSelectionName(id);
-    if (id === "Liquors") {
-      setSelection(liquors);
-    } else if (id === "Beers") {
-      setSelection(beers);
-    } else if (id === "Liqueurs") {
-      setSelection(liqueurs);
-    } else if (id === "Wines") {
-      setSelection(wines);
-    } else if (id === "Mixers") {
-      setSelection(mixers);
-    } else if (id === "Garnishes") {
-      setSelection(garnishes);
-    } else if (id === "Other") {
-      setSelection(other);
-    }
-  }
-
-  function handleInventorySubmit(event) {
+  const handleInventorySubmit = (event) => {
+    event.preventDefault();
     onSubmit();
   }
 
   return (
-    <div className="Form">
+    <div className="form">
       <form id="cocktail-form" className="text-box">
-        <Dropdown choices={categories} choicesName="Ingredient Categories" selectIngredient={handleSelection}/>
-        <Dropdown choices={selection} choicesName={selectionName} selectIngredient={handleChange}/>
-        <button type="submit" className="submission-button" onClick={handleSubmit}>Add ingredients to inventory</button><br/>
-        <br/>
-        <button type="button" className="submission-button" id="submit-inventory" onClick={handleInventorySubmit}>Submit inventory</button>
+        <Dropdown choices={Object.keys(categories)} choicesName="Ingredient Categories" selectIngredient={handleCategoryChange}/>
+        <Dropdown choices={category} choicesName={categoryName} selectIngredient={handleIngredientChange}/>
+        <FormButton onClick={handleIngredientSubmit} text="Add ingredients to inventory" />
+        <FormButton onClick={handleInventorySubmit} text="Submit inventory" />
       </form>
     </div>
   );
