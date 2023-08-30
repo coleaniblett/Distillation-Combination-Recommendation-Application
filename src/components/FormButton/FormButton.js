d65e6e99b4054f60b50d8cbc7c4b08dc49799af8
ColeANiblett <coleaniblett@gmail.com>
2023-08-30 15:26:42 -0700

    center button text

--- src/components/FormButton/FormButton.js
+++ src/components/FormButton/FormButton.js
@@ -4,7 +4,11 @@ import React from 'react';
 export const FormButton = ({ onClick, text }) => {
   return (
     <div className="formButton">
-      <button type="submit" className="submission-button" onClick={onClick}>{text}</button>
+      <button type="submit" className="submission-button" onClick={onClick}>
+        <div class="button-text-wrapper">
+          {text}
+        </div>
+      </button>
       <br />
     </div>
   )
